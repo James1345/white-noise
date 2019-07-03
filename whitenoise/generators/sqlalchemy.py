223bc6f9ae113a3758e446807b3433ae0083bb85
Shriganesh Giri <sgiri@nvidia.com>
2019-07-03 13:26:32 -0700

    unique maps

--- whitenoise/generators/sqlalchemy.py
+++ whitenoise/generators/sqlalchemy.py
@@ -35,18 +35,32 @@ class LinkGenerator(BaseGenerator):
     If the query returns more than one option, either random or the 1st is selected
     (default is random)
     '''
-    def __init__(self, model, max_map=1, random=True, *args, **kwargs):
+    _query = []
+    _chosen_map = []
+
+    def __init__(self, model, max_map=1, unique_maps=False, random=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.session = None
         self.model = model
         self.random = random
         self.max_map = max_map 
+        self.unique_maps = unique_maps
 
     def generate(self):
         if(self.session is None):
             raise ValueError('You must set the session property before using this generator')
-        _query = self.session.query(self.model).all()
+        if not LinkGenerator._query:
+            LinkGenerator._query = self.session.query(self.model).all()
         if self.random:
-            return random.SystemRandom().sample(_query,random.randint(1, self.max_map))
+            if self.unique_maps:
+                while True:
+                    iterList = random.SystemRandom().sample(LinkGenerator._query,random.randint(1, self.max_map))
+                    iterCheck = any(elem in iterList for elem in LinkGenerator._chosen_map)
+                    if not iterCheck:
+                        LinkGenerator._chosen_map.extend(iterList)
+                        break
+                return iterList 
+            else:
+                return random.SystemRandom().sample(LinkGenerator._query,random.randint(1, self.max_map))
         else:
-            return [_query[0]]
+            return [LinkGenerator._query[0]]
